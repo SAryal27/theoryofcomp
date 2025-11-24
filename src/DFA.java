caa499401451377209467aabcd15bf6258ddacfe
Rafael Copado <rafaelcopado30@gmail.com>
2025-11-23 17:45:02 -0800

    added the input requirements to the DFA class

--- src/DFA.java
+++ src/DFA.java
@@ -1,3 +1,5 @@
-public class DFA {
-    
+public class DFA {//extends JPanel to make it visible
+    DFA(String input, String StartState, String[] accepStates, String[][] transitions){//transition contains the table from the front end
+
+    }
 }
